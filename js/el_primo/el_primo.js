fb91f0119f7d32513a22d161ab9514a33872dbd7
MarcOrtizBurgos <71668593+MarcOrtizBurgos@users.noreply.github.com>
2024-04-27 00:28:36 +0200

    movimiento números 3

--- js/el_primo/el_primo.js
+++ js/el_primo/el_primo.js
@@ -1,12 +1,55 @@
 window.addEventListener('load', function () {
-  function handleScroll() {
-    var container = document.querySelector('.background-world');
-    var cloudContainer = document.querySelector('.cloud-container');
-    var scrollPercentage = window.scrollY / (document.body.scrollHeight - window.innerHeight);
-    var rotation = scrollPercentage * 360;
+  const container = document.querySelector('.background-world');
+  const cloudContainer = document.querySelector('.cloud-container');
+  const primoNumber1 = document.querySelector('.primo-number1');
+  const primoNumber2 = document.querySelector('.primo-number2');
+  const primoNumber3 = document.querySelector('.primo-number3');
+  const primoNumber4 = document.querySelector('.primo-number4');
 
-    container.style.transform = 'rotate(' + rotation + 'deg)';
-    cloudContainer.style.transform = 'rotate(' + (-rotation / 2) + 'deg)';
+  function handleScroll() {
+    const scrollPercentage = window.scrollY / (document.body.scrollHeight - window.innerHeight);
+    const rotation = scrollPercentage * 360;
+
+    container.style.transform = `rotate(${rotation}deg)`;
+    cloudContainer.style.transform = `rotate(${-rotation / 2}deg)`;
+
+    const scrollDirection = getScrollDirection();
+    adjustElementPositions(scrollDirection);
+  }
+
+  function getScrollDirection() {
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 700) {
+      return 'bottom';
+    } else if (scrollPosition > 300) {
+      return 'top';
+    } else {
+      return 'middle';
+    }
+  }
+
+  function adjustElementPositions(scrollDirection) {
+    switch (scrollDirection) {
+      case 'top':
+        primoNumber1.style.cssText = 'top: 21%; left: 19%;';
+        primoNumber2.style.cssText = 'top: 18%; right: 12%;';
+        primoNumber3.style.cssText = 'bottom: 45%; left: 45%;';
+        primoNumber4.style.cssText = 'bottom: 45%; right: 45%;';
+        break;
+      case 'bottom':
+        primoNumber1.style.cssText = 'top: 21%; left: 19%;';
+        primoNumber2.style.cssText = 'top: 18%; right: 12%;';
+        primoNumber3.style.cssText = 'bottom: -20%; left: 5%;';
+        primoNumber4.style.cssText = 'bottom: -5%; right: 15%;';
+        break;
+      default:
+        primoNumber1.style.cssText = 'top: 45%; left: 45%;';
+        primoNumber2.style.cssText = 'top: 45%; right: 45%;';
+        primoNumber3.style.cssText = 'bottom: 45%; left: 45%;';
+        primoNumber4.style.cssText = 'bottom: 45%; right: 45%;';
+        break;
+    }
   }
 
   window.addEventListener('scroll', handleScroll);
