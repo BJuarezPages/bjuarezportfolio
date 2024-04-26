4b08dfafb2a16504f114f7d6cbf7b0c8cd94fb60
MarcOrtizBurgos <71668593+MarcOrtizBurgos@users.noreply.github.com>
2024-04-26 20:08:40 +0200

    movimiento nubes al reves

--- js/el_primo/el_primo.js
+++ js/el_primo/el_primo.js
@@ -1,10 +1,12 @@
 window.addEventListener('load', function () {
   function handleScroll() {
     var container = document.querySelector('.background-world');
+    var cloudContainer = document.querySelector('.cloud-container');
     var scrollPercentage = window.scrollY / (document.body.scrollHeight - window.innerHeight);
     var rotation = scrollPercentage * 360;
 
     container.style.transform = 'rotate(' + rotation + 'deg)';
+    cloudContainer.style.transform = 'rotate(' + (-rotation / 2) + 'deg)';
   }
 
   window.addEventListener('scroll', handleScroll);
